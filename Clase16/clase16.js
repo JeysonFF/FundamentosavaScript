53baf5d3e5b6d236de2e0e5fb0e0b9b5fa5f7646
SC <jesuscastellanospaez@hotmail.com>
2019-09-19 23:58:49 -0500

    Clase 16, uso de switch

---
+++ Clase16/clase16.js
@@ -0,0 +1,13 @@
+var signo = prompt('¿Cúal es tu signo?');
+
+
+switch(signo){
+    case 'acuario':
+        console.log(`${signo}`)
+        break;
+    case 'picis':
+        console.log(`${signo}`)
+        break;
+    default:
+        console.log(`el signo "${signo}", no existe`)
+}
