2ba620eae70f8f13a6fe35ae30c1f0f7eca2dea4
SC <jesuscastellanospaez@hotmail.com>
2019-09-21 05:55:04 -0500

    pasar funciones como parametros js clase26

--- Clase26/clase26.js
+++ Clase26/clase26.js
@@ -5,8 +5,14 @@ class Persona{
         this.edad = edad;
         this.estatura = estatura;
     }
-    saludar(){
-        console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+    saludar(fn){
+        var {nombre, apellido} = this;
+        console.log(`Hola me llamo ${nombre} ${apellido}`)
+        if(fn){
+             responderSaludo(nombre, apellido, false);
+        }else{
+
+        }
     }
 
     soyAlto(){
@@ -19,14 +25,28 @@ class Desarrollador extends Persona {
         super(nombre, apellido, edad, estatura);
     }
 
-    saludar() {
+    saludar(fn) {
         console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador@`);
+        if(fn){
+            responderSaludo(this.nombre, this.apellido, true);
+       }else{
+
+       }
     }
 }
 
-
+function responderSaludo(nombre, apellido, esDev){
+    console.log(`Buen día ${nombre} ${apellido}`);
+    if(esDev){
+        console.log('ah mirá, no sabia que eras desarrollador/a');
+    }
+}
 
 var jesus = new Persona('Jesus', 'Castellanos', 29, 1.72);
 var erika = new Persona('Erika', 'Luna', 33, 1.65);
 //var arturo = new Persona('Arturo', 'Martinez', 85, 1.89);
 var arturo = new Desarrollador('Arturo', 'Martinez', 85, 1.89);
+
+jesus.saludar(responderSaludo);
+erika.saludar();
+arturo.saludar(responderSaludo);
