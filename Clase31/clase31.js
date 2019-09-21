4caaf8ab04318e54c5da4448ae0076116201b3c4
SC <jesuscastellanospaez@hotmail.com>
2019-09-21 16:13:35 -0500

    Se realiza el refact con funciones flecha, se deja mas legible el codigo.

--- Clase31/clase31.js
+++ Clase31/clase31.js
@@ -3,35 +3,26 @@ const PEOPLE_URL ='people/:id';
 
 const opts = { crossDomain: true};
 
-function obtenerPersonaje(id, callback){
+const obtenerPersonaje = (id, callback)=>{
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
-   
-    $.get(url, opts, callback)
-    .fail(() => console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`));
+    $.get(url, opts, callback).fail(() => console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`));
 }
 
-obtenerPersonaje(1,function (data){
-    console.log(`Hola, yo soy ${data.name}`)
-
-    obtenerPersonaje(2, function(data){
-        console.log(`Hola, yo soy ${data.name}`)
-
-        obtenerPersonaje(3,function(data){
-            console.log(`Hola, yo soy ${data.name}`)
-
-            obtenerPersonaje(4, function(data){
-                console.log(`Hola, yo soy ${data.name}`)
-
-                obtenerPersonaje(5, function(data){
-                    console.log(`Hola, yo soy ${data.name}`)
-
-                    obtenerPersonaje(6, function(data){
-                        console.log(`Hola, yo soy ${data.name}`)
-
-                        obtenerPersonaje(7, function(data){
-                            console.log(`Hola, yo soy ${data.name}`)
+obtenerPersonaje(1, (data)=>{
+    console.log(`Hola, yo soy ${data.name}`);
+    obtenerPersonaje(2, (data)=>{
+        console.log(`Hola, yo soy ${data.name}`);
+        obtenerPersonaje(3,(data)=>{
+            console.log(`Hola, yo soy ${data.name}`);
+            obtenerPersonaje(4, (data)=>{
+                console.log(`Hola, yo soy ${data.name}`);
+                obtenerPersonaje(5, (data)=>{
+                    console.log(`Hola, yo soy ${data.name}`);
+                    obtenerPersonaje(6, (data)=>{
+                        console.log(`Hola, yo soy ${data.name}`);
+                        obtenerPersonaje(7, (data)=>{
+                            console.log(`Hola, yo soy ${data.name}`);
                         });
-                    
                     })
                 }) 
             })
