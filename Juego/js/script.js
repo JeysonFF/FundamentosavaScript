b7dcb2d741ab44acdbb56d085dfb9004c15c38a7
SC <jesuscastellanospaez@hotmail.com>
2019-09-22 10:07:04 -0500

    Se agrega la captura de los clicks que da el usuario y se entendio como usar this para no perder la referencia del mismo

--- Juego/js/script.js
+++ Juego/js/script.js
@@ -1,17 +1,20 @@
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-const btnEmpezar = document.getElementById('btnEmpezar')
+const celeste = document.getElementById('celeste');
+const violeta = document.getElementById('violeta');
+const naranja = document.getElementById('naranja');
+const verde = document.getElementById('verde');
+const btnEmpezar = document.getElementById('btnEmpezar');
+
 class Juego {
     constructor() {
         this.inicializar();
         this.generarSecuencia();
+        this.siguienteNivel();
     }
     
     inicializar() {
+        this.elegirColor=this.elegirColor.bind(this);
         btnEmpezar.classList.add('hide');
-        this.level = 1;
+        this.level = 5;
         this.colores = {
             celeste,
             violeta,
@@ -22,6 +25,51 @@ class Juego {
 
     generarSecuencia (){
         this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4));
+    }
+
+    siguienteNivel (){
+        this.iluminarSecuencia();
+        this.agregarEventosClick();
+    }
+
+    transformarNumeroAColor(numero){
+        switch (numero){
+            case 0:
+                return 'celeste';
+            case 1:
+                return 'violeta';
+            case 2:
+                return 'naranja';
+            case 3:
+                return 'verde';
+        }
+    }
+
+    iluminarSecuencia (){
+        for(var i=0; i < this.level; i++){
+            const color = this.transformarNumeroAColor(this.secuencia[i]);
+            setTimeout(() => {this.iluminarColor(color)}, 1000 * i);
+        }
+    }
+
+    iluminarColor(color){
+        this.colores[color].classList.add('light');
+        setTimeout(()=> this.apagarColor(color), 350);
+    }
+
+    apagarColor(color){
+        this.colores[color].classList.remove('light');
+    }
+
+    agregarEventosClick(){
+        this.colores.celeste.addEventListener('click', this.elegirColor);
+        this.colores.violeta.addEventListener('click', this.elegirColor);
+        this.colores.naranja.addEventListener('click', this.elegirColor);
+        this.colores.verde.addEventListener('click', this.elegirColor);
+    }
+
+    elegirColor(ev){
+        console.log(this);
     }
 }
    
