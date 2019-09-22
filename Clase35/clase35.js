67512a1613c037cac3af03fd66e866c34ad00522
SC <jesuscastellanospaez@hotmail.com>
2019-09-21 20:27:49 -0500

    Async-await: lo último en asincronismo Async-await es la manera más simple y clara de realizar tareas asíncronas. Await detiene la ejecución del programa hasta que todas las promesas sean resueltas. Para poder utilizar esta forma, hay que colocar async antes de la definición de la función, y encerrar el llamado a Promises.all() dentro de un bloque try … catch.

---
+++ Clase35/clase35.js
@@ -0,0 +1,29 @@
+const API_URL='https://swapi.co/api/';
+const PEOPLE_URL ='people/:id';
+
+const opts = { crossDomain: true};
+
+const obtenerPersonaje = (id)=>{
+    return new Promise( (resolve, reject) =>{
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+        $
+        .get(url, opts, (data)=>{
+            resolve(data);
+        })
+        .fail(()=>reject(id));
+   })};
+
+const onError = (id)=>console.log(`Sucedió un error al obtener el personaje ${id}`);
+
+async function getPersonajes(){
+    var ids = [1,2,3,4,5,6,7];
+    var promises = ids.map((id) => obtenerPersonaje(id));
+    try {
+        var data = await Promise.all(promises);    
+        console.log(data);
+    } catch (id) {
+        onError(id);
+    }
+}
+
+getPersonajes();
